1e4edaeae2f1a17a0bcbd864bd6dac31e766df5a
Kaj Bromose <kbr@ucl.dk>
2025-08-21 10:30:35 +0200

    Indkapsling demo

---
+++ DemoKode/IndkapslingDemo/IndkapslingDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var person = new Person("TEst") as IPerson;
+person.Hello();
+
+
+interface IPerson
+{
+    void Hello();
+}
+
+public class Person : IPerson
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+   public string Name { get; private set; }
+
+     void IPerson.Hello()
+    {
+        Console.WriteLine($"Hello - My name is {Name}");
+    }
+}
