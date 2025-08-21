ed95142e4b9901a97efa804c128f8eba535e7338
Kaj Bromose <kbr@ucl.dk>
2025-08-21 11:19:42 +0200

    Maler demo kode

--- DemoKode/IndkapslingDemo/IndkapslingDemo/Program.cs
+++ DemoKode/IndkapslingDemo/IndkapslingDemo/Program.cs
@@ -1,25 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
 
-var person = new Person("TEst") as IPerson;
+var person = Factory(1);
+person.Hello();
+person = Factory(2);
 person.Hello();
 
-
-interface IPerson
+ IPerson Factory(int type)
+ {
+     if (type == 1) return new Person("Person");
+     return new PersonChild("Child");
+ }
+    interface IPerson
 {
     void Hello();
 }
 
+public class PersonChild : IPerson
+{
+    public string Name { get; }
+
+    public PersonChild(string name)
+    {
+        Name = name;
+    }
+
+    void IPerson.Hello()
+    {
+        Console.WriteLine($"Hello - My name is {Name} and I am under age");
+    }
+}
+
 public class Person : IPerson
 {
+    public string Name { get; }
+
     public Person(string name)
     {
         Name = name;
     }
-   public string Name { get; private set; }
 
-     void IPerson.Hello()
+    void IPerson.Hello()
     {
         Console.WriteLine($"Hello - My name is {Name}");
+    }
+}
+
+public class PersonFake : IPerson
+{
+    void IPerson.Hello()
+    {
+        Console.WriteLine("I'm a fake");
     }
 }
---
+++ DemoKode/Maler/MalerConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+IRabatBeregner beregner;
+var ordre = new Order { Kunde = new Kunde { KundeType = KundeType.Privat }, OrdreSum = 1000};
+beregner = Factory(ordre.Kunde.KundeType);
+
+IRabatBeregner Factory(KundeType kundeType)
+{
+    if (kundeType == KundeType.Privat) return new PrivatKundeRabat();
+}
+
+beregner.BeregnRabat(ordre);
+
+public interface IRabatBeregner
+{
+    double BeregnRabat(Order ordre);
+}
+
+public class PrivatKundeRabat : IRabatBeregner
+{
+    /// <summary>
+    /// Hvis ordresummen er over 1.000,- gives en rabat på 10% af ordresummen.
+    /// Hvis ordresummen er over 5.000,- gives en rabat på 15% af ordresummen.
+    /// Hvis ordresummen er over 10.000,- gives en rabat på 20% af ordresummen.
+    /// </summary>
+    /// <param name="ordre"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+        double IRabatBeregner.BeregnRabat(Order ordre)
+        {
+            if (ordre.OrdreSum > 10000) return ordre.OrdreSum * 0.2;
+            if (ordre.OrdreSum > 5000) return ordre.OrdreSum * 0.15;
+            if (ordre.OrdreSum > 1000) return ordre.OrdreSum * 0.10;
+
+            return 0;
+        }
+}
+
+public class Kunde
+{
+    public KundeType KundeType { get; set; }
+}
+
+public enum KundeType
+{
+    Privat,
+    Virksomhed
+}
+
+public class Order
+{
+    public Kunde Kunde { get; set; }
+    public double OrdreSum { get; set;}
+}
