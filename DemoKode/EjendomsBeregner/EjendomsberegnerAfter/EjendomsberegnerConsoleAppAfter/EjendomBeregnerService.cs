a65e3da42a6e93f3d0c489b4b1b1fa80c8782744
Kaj Bromose <kbr@ucl.dk>
2025-08-28 11:33:57 +0200

    Ejendomsberegner after added

---
+++ DemoKode/EjendomsBeregner/EjendomsberegnerAfter/EjendomsberegnerConsoleAppAfter/EjendomBeregnerService.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EjendomsberegnerConsoleAppAfter;
+/// <summary>
+/// Kan funktionaliteten opdeles således koden bliver mere funktionel "ren" (en metode ét ansvar)?
+///  Er der et skjult model objekt i opgaven?
+///  Hvad skal retur typen for adapteren være?
+/// </summary>
+public class EjendomBeregnerService
+{
+
+    private ILejemaalRepository _repo;
+
+    public EjendomBeregnerService(ILejemaalRepository repo)
+    {
+        _repo = repo;
+    }
+
+    /// <summary>
+    ///     Beregner ejendommens kvadratmeter ud fra ejendommens lejelmål.
+    ///     Lejemål er i en simikolon separeret tekstfil. Formatet af filen er:
+    ///     lejlighednummer; kvadratmeter; antal rum
+    ///     lejlighednummer: int
+    ///     kvadratmeter: double
+    ///     antal rum: double
+    /// 
+    ///     Første linje i filen er en header og skal ignoreres.
+    /// 
+    ///     Filen med lejemål er med i projektet og hedder "LejemaalData.csv"
+    /// </summary>
+    /// <returns></returns>
+    public double BeregnKvadratmeter()
+    {
+        var lejemaalene = _repo.HentLejemaal();
+        double kvadratmeter = 0.0;
+
+
+        foreach (Lejemaal lejemaal in lejemaalene)
+        {
+            kvadratmeter += lejemaal.Kvadratmeter;
+        }
+
+        return kvadratmeter;
+    }
+
+
+
+
+
+
+    public class Lejemaal
+    {
+        public int Lejlighednummer { get; set; }
+        public double Kvadratmeter { get; set; }
+        public int AntalRum { get; set; }
+    }
+
+    public interface ILejemaalRepository
+    {
+        List<Lejemaal> HentLejemaal();
+    }
+
+    public class LejemaalFraFilRepository : ILejemaalRepository
+    {
+        public string DataFileName { get; set; } = "LejemaalData.csv";
+        List<Lejemaal> ILejemaalRepository.HentLejemaal()
+        {
+            var raaData = File.ReadAllLines(DataFileName).Skip(1).ToArray();
+            var lejemaal = Konverter(raaData);
+            return lejemaal;
+        }
+
+        private List<Lejemaal> Konverter(string[] data)
+        {
+            var lejemaalListe = new List<Lejemaal>();
+            foreach (string lejemaal in data)
+            {
+                string[] lejemaalParts = lejemaal.Split(';');
+                double lejemaalKvadratmeter;
+                int lejemaalNummer;
+                int antalRum;
+                double.TryParse(RemoveQuotes(lejemaalParts[1]), out lejemaalKvadratmeter);
+                int.TryParse(RemoveQuotes(lejemaalParts[0]), out lejemaalNummer);
+                int.TryParse(RemoveQuotes(lejemaalParts[2]), out antalRum);
+
+                lejemaalListe.Add(new Lejemaal
+                {
+                    Lejlighednummer = lejemaalNummer,
+                    Kvadratmeter = lejemaalKvadratmeter,
+                    AntalRum = antalRum
+                });
+            }
+
+            return lejemaalListe;
+        }
+        private string RemoveQuotes(string lejemaalPart)
+        {
+            return lejemaalPart.Replace('"', ' ').Trim();
+        }
+    }
+
+}
---
+++ DemoKode/EjendomsBeregner/EjendomsberegnerAfter/EjendomsberegnerConsoleAppAfter/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EjendomsberegnerConsoleAppAfter;
+
+Console.WriteLine("Hello, World!");
+var beregnerService = new EjendomBeregnerService();
+double kvadratmeter = beregnerService.BeregnKvadratmeter();
+Console.WriteLine($"Ejendommens samlede kvadratmeter er: {kvadratmeter}");
