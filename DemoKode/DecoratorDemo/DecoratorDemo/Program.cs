119dabf48f4964f1bc27336030e0d4134db3752d
Kaj Bromose <kbr@ucl.dk>
2025-08-28 08:03:25 +0200

    Decoreator demo added

---
+++ DemoKode/DecoratorDemo/DecoratorDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Brug
+IShape circle = new Circle();
+IShape redCircle = new RedBorderDecorator(circle);
+redCircle.Draw();
+
+
+interface IShape
+{
+    void Draw();
+}
+
+class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Circle");
+    }
+}
+
+abstract class ShapeDecorator : IShape
+{
+    protected IShape decoratedShape;
+    public ShapeDecorator(IShape shape)
+    {
+        decoratedShape = shape;
+    }
+    public virtual void Draw()
+    {
+        decoratedShape.Draw();
+    }
+}
+
+class RedBorderDecorator : ShapeDecorator
+{
+    public RedBorderDecorator(IShape shape) : base(shape) { }
+    public override void Draw()
+    {
+        base.Draw();
+        Console.WriteLine("Adding Red Border");
+    }
+}
+
+
