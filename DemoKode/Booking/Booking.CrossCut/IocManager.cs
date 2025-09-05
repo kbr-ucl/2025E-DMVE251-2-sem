b6fbd30806b7c08839d69333948201f59f08f77c
Kaj Bromose <kbr@ucl.dk>
2025-09-05 09:37:47 +0200

    Core er klar

---
+++ DemoKode/Booking/Booking.CrossCut/IocManager.cs
@@ -0,0 +1,24 @@
+﻿using Booking.Application;
+using Booking.Infrastructor;
+using Booking.Domain.DomainService;
+using Microsoft.Extensions.DependencyInjection;
+namespace Booking.CrossCut
+{
+    public class IocManager
+    {
+        private static ServiceProvider? ServiceProvider;
+        public static ServiceProvider RegisterService()
+        {
+            if (ServiceProvider is not null) return ServiceProvider;
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<IBookingCommand, BookingCommandHandler>()
+                .AddScoped<IBookingRepository, BookingRepoDummy>()
+                .AddScoped<IBookingOverlapCheck, BookingOverlapCheck>()
+                .BuildServiceProvider();
+
+            ServiceProvider = serviceProvider;
+            return ServiceProvider;
+
+        }
+    }
+}
---
+++ DemoKode/Booking/Booking.Infrastructor/BookingOverlapCheck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Booking.Domain.DomainService;
+
+namespace Booking.Infrastructor
+{
+    public class BookingOverlapCheck : IBookingOverlapCheck
+    {
+        bool IBookingOverlapCheck.IsOverlapping(Domain.Entity.Booking booking)
+        {
+            return false;
+        }
+    }
+}
