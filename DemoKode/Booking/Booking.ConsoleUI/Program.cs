d94dc878915cd557341a211b674a667f6a6f344c
Kaj Bromose <kbr@ucl.dk>
2025-09-05 09:56:24 +0200

    UI med BookingCommand done

---
+++ DemoKode/Booking/Booking.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Booking.Application;
+using Booking.CrossCut;
+
+Console.WriteLine("Hello, World!");
+
+var serviceProvider = IocManager.RegisterService();
+var bookingCommand = serviceProvider.GetService(typeof(IBookingCommand)) as IBookingCommand;
+
+bookingCommand.UpdateStartTid(DateTime.Now 
+                              + new TimeSpan(0,0,30,0));
+
+Console.WriteLine("Done");
