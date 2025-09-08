0a12bf29fde973e98a60817f40fd1dc822319f43
Kaj Bromose <kbr@ucl.dk>
2025-09-08 14:50:26 +0200

    Refactor IoCManager (preparing for Blazor)

---
+++ DemoKode/Booking/Booking.CrossCut/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Booking.Application;
+using Booking.Domain.DomainService;
+using Booking.Infrastructor;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booking.CrossCut;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBookingCore(this IServiceCollection services)
+    {
+        services.AddScoped<IBookingCommand, BookingCommandHandler>();
+        services.AddScoped<IBookingRepository, BookingRepoDummy>();
+        services.AddScoped<IBookingOverlapCheck, BookingOverlapCheck>();
+        return services;
+    }
+}
--- DemoKode/Booking/Booking.CrossCut/IocManager.cs
+++ DemoKode/Booking/Booking.CrossCut/IocManager.cs
@@ -1,24 +1,19 @@
-﻿using Booking.Application;
-using Booking.Infrastructor;
-using Booking.Domain.DomainService;
-using Microsoft.Extensions.DependencyInjection;
-namespace Booking.CrossCut
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Booking.CrossCut;
+
+public class IocManager
 {
-    public class IocManager
+    private static ServiceProvider? ServiceProvider;
+
+    public static ServiceProvider RegisterService()
     {
-        private static ServiceProvider? ServiceProvider;
-        public static ServiceProvider RegisterService()
-        {
-            if (ServiceProvider is not null) return ServiceProvider;
-            var serviceProvider = new ServiceCollection()
-                .AddScoped<IBookingCommand, BookingCommandHandler>()
-                .AddScoped<IBookingRepository, BookingRepoDummy>()
-                .AddScoped<IBookingOverlapCheck, BookingOverlapCheck>()
-                .BuildServiceProvider();
+        if (ServiceProvider is not null) return ServiceProvider;
+        var serviceProvider = new ServiceCollection()
+            .AddBookingCore()
+            .BuildServiceProvider();
 
-            ServiceProvider = serviceProvider;
-            return ServiceProvider;
-
-        }
+        ServiceProvider = serviceProvider;
+        return ServiceProvider;
     }
 }
