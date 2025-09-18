630d5f8cef254ceb37c98c623ba5a87efdbe22a5
Kaj Bromose <kbr@ucl.dk>
2025-09-18 15:01:31 +0200

    Booking: OVerlap ansvar tilbage i Booking

--- DemoKode/Booking/Booking.Domain/Entity/Booking.cs
+++ DemoKode/Booking/Booking.Domain/Entity/Booking.cs
@@ -1,4 +1,5 @@
-﻿using Booking.Domain.Exceptions;
+﻿using Booking.Domain.DomainService;
+using Booking.Domain.Exceptions;
 
 namespace Booking.Domain.Entity;
 
@@ -6,24 +7,36 @@ public class Booking : EntityBase
 {
     public DateTime StartTid { get; private set; }
     public DateTime SlutTid { get; private set; }
-    public Kunde Kunde { get; private set; }
+    public Kunde Kunde { get; }
 
     // EF Only
-    protected Booking() { }
+    protected Booking()
+    {
+    }
 
-    public Booking(DateTime start, DateTime slut, Kunde kunde)
+    public Booking(DateTime start, DateTime slut, Kunde kunde, IBookingOverlapCheck overlapCheck)
     {
         if (kunde is null) throw new ArgumentNullException(nameof(kunde));
         ValidateTimes(start, slut);
+        OverlapCheck(overlapCheck, kunde.Id, start, slut);
 
         Kunde = kunde;
         StartTid = start;
         SlutTid = slut;
     }
 
-    public void UpdateStartSlut(DateTime start, DateTime slut)
+    private void OverlapCheck(IBookingOverlapCheck overlapCheck, int kundeId, DateTime start, DateTime slut,
+        int? excludeBookingId = null)
+    {
+        // Validate business rule (no overlap)
+        if (overlapCheck.HasOverlap(kundeId, start, slut))
+            throw new ValidationException("The booking overlaps an existing booking.");
+    }
+
+    public void UpdateStartSlut(DateTime start, DateTime slut, IBookingOverlapCheck overlapCheck)
     {
         ValidateTimes(start, slut);
+        OverlapCheck(overlapCheck, Kunde.Id, start, slut, Id);
         StartTid = start;
         SlutTid = slut;
     }
